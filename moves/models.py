f4fabec9700660e1ec3ea999fe7bf8ccf594e2bf
Ben <benjamin.c.henderson@gmail.com>
2015-09-05 13:43:58 -0400

    add models

--- moves/models.py
+++ moves/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Spot(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Move(models.Model):
+    user = models.CharField(max_length=50)
+    spot = models.ForeignKey(Spot)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
