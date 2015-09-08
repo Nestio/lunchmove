ab953233df71a65e62988526363b117218d0ff05
Ben <benjamin.c.henderson@gmail.com>
2015-09-07 22:27:31 -0400

    edit settings and admin

--- lunchmove/settings.py
+++ lunchmove/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tt-sz7=q@x5jked9f3(%2p^=#=9=imk*_it!tx!x7rlxd=)k*d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
@@ -97,7 +97,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
--- moves/admin.py
+++ moves/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Move, Spot
 
-# Register your models here.
+admin.site.register(Move)
+admin.site.register(Spot)
