e7b99f9993fa74377a72cac5295ffe6cb6b1d717
Joseph A. Daly <josefdaly@gmail.com>
2015-11-23 17:45:50 -0500

    Create IndexTests class. Implement uuid test.

--- moves/tests.py
+++ moves/tests.py
@@ -1,3 +1,10 @@
+import pdb
 from django.test import TestCase
+from .models import Move
 
 # Create your tests here.
+class IndexTests(TestCase):
+
+    def test_user_uuid_is_set_if_not_set(self):
+        self.client.get('/')
+        self.assertTrue(self.client.session.get('user_uuid'))
