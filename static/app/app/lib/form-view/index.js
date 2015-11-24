43ee9854817a3586c7bb9908d5cdf26e0d0281a6
Ben <benjamin.c.henderson@gmail.com>
2015-11-24 11:18:48 -0800

    form view tests

--- static/app/app/lib/form-view/index.js
+++ static/app/app/lib/form-view/index.js
@@ -8,6 +8,8 @@ Backbone.Syphon = require('backbone.syphon');
 require('backbone.radio');
 require('backbone.babysitter');
 var Marionette = require('backbone.marionette');
+require('bootstrap/js/tooltip');
+require('bootstrap/js/popover');
 
 // App
 var fs = require('fs');
@@ -35,7 +37,7 @@ InputWriterSet.registerDefault(function(el, value){
     }
 });
 
-InputReaderSet.registerDefault(function(el){    
+InputReaderSet.registerDefault(function(el){
     return el.val();
 });
 
@@ -92,7 +94,7 @@ var FormView = Marionette.ItemView.extend({
 
     // Object of fields
     fields: {},
-    
+
     // (Optional) Widgets which will apply to groups of inputs
     inputGroups: {},
 
@@ -193,7 +195,7 @@ var FormView = Marionette.ItemView.extend({
         // Returns the input field el
         return this.$('[data-field="'+fieldName+'"]');
     },
-    
+
     getInputGroupEl: function(fieldName){
         // Returns the input group el
         return this.$('[data-input-group="' + fieldName + '"]');
@@ -212,13 +214,13 @@ var FormView = Marionette.ItemView.extend({
         }, this);
 
         this.className = this._className + ' ' + (this.className || '');
-        
+
         // Extend default ui hash
         this.ui = _.extend({}, this._defaultUI, this.ui);
 
         // Extend default events hash
         this.events = _.extend({}, this._defaultEvents, this.events);
-        
+
         Marionette.ItemView.prototype.constructor.apply(this, arguments);
 
         if (!this.model) { this.model = new Backbone.Model(); }
@@ -240,8 +242,8 @@ var FormView = Marionette.ItemView.extend({
         this.once('show', function(){
             this.prepareFormState();
             this.on('render', this.prepareFormState, this);
-        }, this);    
-        
+        }, this);
+
         this.once('before:destroy', this.disablePlugins, this);
         this.once('before:destroy', this.destroyWidgets, this);
     },
@@ -250,7 +252,7 @@ var FormView = Marionette.ItemView.extend({
         if (this._widgetViews.length) {
             this.destroyWidgets();
         }
-        
+
         this.renderInputWidgets();
         this.renderInputGroupWidgets();
 
@@ -258,10 +260,10 @@ var FormView = Marionette.ItemView.extend({
     },
     // Data Serializers, Encoders, and Cleaners
 
-    _encode: function(data, encodeDirection){        
-        var encodedData = _.mapObject(data, function(val, key){            
+    _encode: function(data, encodeDirection){
+        var encodedData = _.mapObject(data, function(val, key){
             var fieldOptions = this.fields[key];
-            
+
             if (!_.isString(fieldOptions) && !_.isObject(fieldOptions)) {
                 throw new Error('Field options must be a string of the encoder type, or an object of options.');
             }
@@ -276,7 +278,7 @@ var FormView = Marionette.ItemView.extend({
                 encoderName = fieldOptions.encoder.name || encoderName;
                 encoderOptions = _.omit(fieldOptions.encoder, 'name');
             }
-            
+
             var encoder = this.encoders[encoderName];
 
             if (encoder) {
@@ -313,13 +315,13 @@ var FormView = Marionette.ItemView.extend({
         this.deserializing = true;
 
         Backbone.Syphon.deserialize(this, decodedData, {include: inputElKeys});
-        
+
         this.deserializing = false;
         this.triggerMethod('deserialize');
     },
     _getFieldKeys: function(options){
         options = options || {};
-        
+
         // Convert fields to an array of key
         return _.reduce(this.fields, function(keys, val, key){
             if (val.nested) {
@@ -391,7 +393,7 @@ var FormView = Marionette.ItemView.extend({
         // If rule is an object, get first property for name/values (only one key/val set allowed)
         var ruleName = _.isObject(rule) ? _.first(_.keys(rule)) : rule;
 
-        // If not required, not a local rule, and value is null/empty, do not validate. 
+        // If not required, not a local rule, and value is null/empty, do not validate.
         if (ruleName !== 'required' && !this.rules[ruleName] && !value) {
             return;
         }
@@ -440,7 +442,7 @@ var FormView = Marionette.ItemView.extend({
         return null;
     },
 
-    _cleanFormData: function(data){        
+    _cleanFormData: function(data){
         if (this.getOption('hasFiles')) {
             // Remove file fields from serialized data
             _.each(this.fields, function(field, fieldName){
@@ -457,7 +459,7 @@ var FormView = Marionette.ItemView.extend({
         // Unset non-attribute values saved to model on an iframe submit
         _.each(['_method','csrfmiddlewaretoken'], function(fieldName){
             model.unset(fieldName, {silent: true});
-        });     
+        });
     },
 
     // Submit Handlers & Methods
@@ -486,11 +488,11 @@ var FormView = Marionette.ItemView.extend({
 
     onSubmit: function(e, data){
         e.preventDefault();
-        
+
         this.clearAlerts();
         this.clearErrors();
         this.disableSaving();
-        
+
         data = this._cleanFormData(data);
 
         var options = {
@@ -505,7 +507,7 @@ var FormView = Marionette.ItemView.extend({
                     this.handleSubmitError(responseData);
                     return;
                 }
-                
+
                 this.handleSubmitSuccess();
                 this.triggerMethod('submit:success');
             }, this),
@@ -544,7 +546,7 @@ var FormView = Marionette.ItemView.extend({
     disableSaving: function(){
         var defaultText = this.ui.saveButton.text();
         var savingText = this.ui.saveButton.data('saving-text') || defaultText;
-        
+
         this.ui.saveButton
             .prop('disabled', true)
             .addClass('disabled').blur()
@@ -577,7 +579,7 @@ var FormView = Marionette.ItemView.extend({
     handleSubmitError: function(response){
         this.enableSaving();
         var errorAlert = {alertType: 'error', message: this.messages.saveError};
-        
+
         if (response && response.status_code >= 400) {
             // Form has validation errors
             this.showErrors(response.errors);
@@ -670,9 +672,9 @@ var FormView = Marionette.ItemView.extend({
                 var widgetName = _.isObject(fieldOptions.widget) ? fieldOptions.widget.name : fieldOptions.widget;
 
                 var WidgetView = this.getOption('inputWidgetViews')[widgetName];
-                
+
                 var widgetEl = this.getFieldEl(key);
-                
+
                 if (WidgetView && widgetEl.length) {
                     var options = _.extend({
                         fieldOptions: _.omit(fieldOptions, 'widget'),
@@ -701,12 +703,12 @@ var FormView = Marionette.ItemView.extend({
         _.each(this.inputGroups, _.bind(function(inputGroupOptions, key){
             var inputGroupName = _.isObject(inputGroupOptions) ? inputGroupOptions.name : inputGroupOptions;
             var WidgetView = this.getOption('inputGroupWidgetViews')[inputGroupName];
-            
+
             if (WidgetView) {
                 var widgetView = new WidgetView(_.extend({
                     el: this.getInputGroupEl(key),
                 }, inputGroupOptions.options));
-                
+
                 widgetView.render();
 
                 // Store widgetView for later destruction
---
+++ static/app/test/specs/edit/views.spec.js
@@ -0,0 +1,132 @@
+// Test
+var assert = require('chai').assert;
+var sinon = require('sinon');
+var Factory = require('factory_girl');
+
+// Dependencies
+var _ = require('underscore');
+var Marionette = require('backbone.marionette');
+var Radio = require('backbone.radio');
+
+// Fixtures
+var fs = require('fs');
+var fixtures = require('fixtures');
+var BaseRegionsFixt = fs.readFileSync(__dirname + '/../../fixtures/base-regions.html', 'utf8');
+
+// App
+var Controller = require('app/controller');
+var EntitiesAPI = require('app/entities').API;
+var NameView = require('app/edit/views').NameView;
+var Move = require('app/entities').Move;
+var Moves = require('app/entities').Moves;
+var Spots = require('app/entities').Spots;
+
+var channel = Radio.channel('global');
+
+describe('Edit', function(){
+    describe('Views', function(){
+        before(function(){
+            fixtures.append(BaseRegionsFixt);
+
+            this.mainRegion = new Marionette.Region({
+                el: '#main-region'
+            });
+
+            this.entitiesAPI = new EntitiesAPI();
+        });
+
+        beforeEach(function(){
+            var spotsData = _.times(10, function(){
+                return Factory.create('Spot').toJSON();
+            });
+            var spots = this.spots = new Spots(spotsData);
+
+            this.replySpotsStub = sinon.stub(this.entitiesAPI, 'replySpots', function(){
+                return spots;
+            });
+
+            this.server = sinon.fakeServer.create();
+        });
+
+        afterEach(function(){
+            this.replySpotsStub.restore();
+            this.server.restore();
+            this.mainRegion.empty();
+        });
+
+        after(function(){
+            fixtures.cleanup();
+        });
+
+        describe('NameView', function(){
+            beforeEach(function(){
+                this.model = new Move();
+
+                this.view = new NameView({
+                    model: this.model
+                });
+
+                this.mainRegion.show(this.view);
+            });
+
+            it('enables the submit button when user name is filled in', function(){
+                assert.isTrue(this.view.ui.saveButton.hasClass('disabled'));
+                this.view.getInputEl('user').val('User Name').trigger('input');
+                assert.isFalse(this.view.ui.saveButton.hasClass('disabled'));
+            });
+
+            it('sets the user name on the model when it is submitted', function(){
+                var userName = 'User Name';
+                this.view.getInputEl('user').val(userName).trigger('input');
+                assert.notOk(this.model.get('user'));
+                this.view.ui.form.submit();
+                assert.equal(this.model.get('user'), userName);
+            });
+
+            it('triggers the edit route when it is submitted', function(){
+                var triggerSpy = sinon.spy(channel, 'trigger');
+                this.view.getInputEl('user').val('user name').trigger('input');
+                assert.notOk(triggerSpy.called);
+                this.view.ui.form.submit();
+                assert.isTrue(triggerSpy.called);
+                assert.isTrue(triggerSpy.calledWith('edit'));
+                triggerSpy.restore();
+            });
+        });
+
+        describe('MoveFormView', function(){
+            beforeEach(function(){
+                this.model = new Move({
+                    user: 'User Name'
+                });
+
+                this.view = new MoveFormView({
+                    model: this.model
+                });
+
+                this.mainRegion.show(this.view);
+            });
+
+            it('enables the submit button when spot and time are filled in');
+
+            it('calls render typeahead on show');
+
+            it('sets the value of spotName on deserialize if spotId has a value');
+
+            //NOTE: you'll need to create a fakeServer response for this to work. See http://sinonjs.org/docs/#server
+            it('triggers list on submit');
+
+            describe('onSpotBlur', function(){
+                it('sets spotId if there is no spotId but val of spotName matches the name of a spot ');
+                it('clears spotName if there is no spotId and the val of spotName does not match the name of a spot');
+                it('sets spotName if there is a spotId');
+            });
+
+            describe('addSpot', function(){
+                //NOTE: you'll need to create a fakeServer response for these to work. See http://sinonjs.org/docs/#server
+                it('adds a new spot to the spots collection with the name of the value of spotName');
+                it('sets spotId to be the value of the newly created spot');
+            });
+        });
+    });
+});
