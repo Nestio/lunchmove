d0411ec5e02dd737a1e27831c11fa6efce15949c
Joseph A. Daly <josefdaly@gmail.com>
2015-11-24 12:29:51 -0500

    Complete move index route six hour test.

--- moves/tests.py
+++ moves/tests.py
@@ -37,3 +37,20 @@ class MoveViewSetTests(TestCase):
         move = json.loads(self.client.get('/json/moves/').content)['results'][0]
         self.assertEqual(move['id'], 1)
         self.assertEqual(move['uuid'], uuid)
+
+    @override_settings(DEBUG=True)
+    def test_moves_index_route_only_displays_moves_from_the_last_six_hours(self):
+        spot = Spot.objects.create(name='The Spot')
+        time_changes = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+        for el in time_changes:
+            time = timezone.now() + datetime.timedelta(hours=el)
+            username = 'user' + str(el)
+            Move.objects.create(user=username, spot=spot, time=time)
+
+        self.client.get('/')
+        moves = json.loads(self.client.get('/json/moves/').content)['results']
+        for move in moves:
+            move_time = datetime.datetime.strptime(move['time'], "%Y-%m-%dT%H:%M:%S.%fZ")
+            if move_time > datetime.datetime.now():
+                difference = move_time - datetime.datetime.now()
+                self.assertFalse(difference.seconds/3600 > 6)
