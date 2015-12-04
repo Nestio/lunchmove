2445962f3cb9890bf6a47459c38d14be9bf8902d
Dan Garfield <dbgarf@gmail.com>
2015-12-04 12:53:04 -0500

    lunchmove with no time set defaults to now

--- moves/models.py
+++ moves/models.py
@@ -23,7 +23,7 @@ class Move(models.Model):
 
     user = models.CharField(max_length=50)
     spot = models.ForeignKey(Spot)
-    time = models.DateTimeField(blank=True, null=True)
+    time = models.DateTimeField(blank=True, null=True, default=datetime.datetime.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     uuid = models.UUIDField(blank=True, null=True)
